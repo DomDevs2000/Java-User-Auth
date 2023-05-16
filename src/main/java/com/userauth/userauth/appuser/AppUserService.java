fdde66cbe21ab9899a6b58f6638a1b90ddc9b553
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-16 15:36:51 +0100

    updated AppUser service

--- src/main/java/com/userauth/userauth/appuser/AppUserService.java
+++ src/main/java/com/userauth/userauth/appuser/AppUserService.java
@@ -1,13 +1,19 @@
 package com.userauth.userauth.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+    private final static String USER_NOT_FOUND_MESSAGE = "User With Email %s Not Found";
+    private final AppUserRepositiory appUserRepositiory;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepositiory.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
     }
 }
