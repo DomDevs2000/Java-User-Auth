8d08a4082ed03505962a7a7fc5f4f81af253038e
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-16 16:01:22 +0100

    added jpa repo extension to class

--- src/main/java/com/userauth/userauth/registration/RegistrationController.java
+++ src/main/java/com/userauth/userauth/registration/RegistrationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
-    private final RegistrationRequest registrationRequest;
     private final RegistrationService registrationService;
 
     public String register(@RequestBody RegistrationRequest request){
--- src/main/java/com/userauth/userauth/registration/RegistrationService.java
+++ src/main/java/com/userauth/userauth/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.userauth.userauth.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "Hello World";
