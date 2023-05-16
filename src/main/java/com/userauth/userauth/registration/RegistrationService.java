cbf7e2d278bc647320c1d2df0d8ea5ab4c8dc4b6
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-16 16:52:44 +0100

    if email is valid, signs up new user

--- src/main/java/com/userauth/userauth/registration/RegistrationService.java
+++ src/main/java/com/userauth/userauth/registration/RegistrationService.java
@@ -1,10 +1,34 @@
 package com.userauth.userauth.registration;
 
+import com.userauth.userauth.appuser.AppUser;
+import com.userauth.userauth.appuser.AppUserRole;
+import com.userauth.userauth.appuser.AppUserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
+
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
+    public RegistrationService(EmailValidator emailValidator, AppUserService appUserService) {
+        this.emailValidator = emailValidator;
+        this.appUserService = appUserService;
+    }
+
     public String register(RegistrationRequest request) {
-        return "Hello World";
+        boolean isValid = emailValidator.test(request.getEmail());
+
+        if (!isValid) {
+            throw new IllegalStateException("Email Not Valid");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+
+        ));
     }
 }
