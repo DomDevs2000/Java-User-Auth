b884b5d9e81a51899b9dda9f220d2ebe51484e44
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-16 16:17:52 +0100

    added post mapping annotation

--- src/main/java/com/userauth/userauth/registration/RegistrationController.java
+++ src/main/java/com/userauth/userauth/registration/RegistrationController.java
@@ -1,17 +1,16 @@
 package com.userauth.userauth.registration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
+
     private final RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
