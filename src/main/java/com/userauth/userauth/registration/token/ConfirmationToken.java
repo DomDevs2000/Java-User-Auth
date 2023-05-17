a68497b82c63c5ce91c0765d5515e111addda87b
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-17 15:55:28 +0100

    initial set up of confirmation token variables

---
+++ src/main/java/com/userauth/userauth/registration/token/ConfirmationToken.java
@@ -0,0 +1,12 @@
+package com.userauth.userauth.registration.token;
+
+import java.time.LocalDateTime;
+
+public class ConfirmationToken {
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime confirmedAt;
+
+}
