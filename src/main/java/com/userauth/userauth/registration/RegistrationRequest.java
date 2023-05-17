0302801439485e51a303cf8e43611e4e9bb25b0b
DomDevs2000 <aidan@carvalho.org.uk>
2023-05-17 12:47:50 +0100

    changed class to record

--- src/main/java/com/userauth/userauth/registration/RegistrationRequest.java
+++ src/main/java/com/userauth/userauth/registration/RegistrationRequest.java
@@ -1,18 +1,5 @@
 package com.userauth.userauth.registration;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
 
-@Getter
-@AllArgsConstructor
-@EqualsAndHashCode
-@ToString
-public class RegistrationRequest {
-    private final String firstName;
-    private final String lastName;
-    private final String password;
-    private final String email;
-
+public record RegistrationRequest(String firstName, String lastName, String password, String email) {
 }
